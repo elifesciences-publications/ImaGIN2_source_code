1e2f16bf0b7f8995fb4e2794baeb091f32010863
Francois Tadel <francois.tadel@univ-grenoble-alpes.fr>
2018-01-09 14:52:01 +0100

    Moved files

--- toolbox/io/ImaGIN_spm_eeg_converteeg2mat.m
+++ toolbox/io/ImaGIN_spm_eeg_converteeg2mat.m
@@ -318,3 +318,28 @@ end
 
 
 
+%% ===== COPY FIELDS =====
+% Copy fields from one structure to another.
+%
+% USAGE:  sDest = ImaGIN_copy_fields(sDest, sSrc, fieldNames) % Copy requested fields
+%         sDest = ImaGIN_copy_fields(sDest, sSrc)             % Copy all fields
+%
+% INPUT:
+%    - sDest : Destination structure 
+%    - sSrc  : Source structure
+%    - fieldNames : Cell array, list of fields to copy from sSrc to sDest
+%
+% Author: Francois Tadel, 2017
+function sDest = ImaGIN_copy_fields(sDest, sSrc, fieldNames)
+    if isempty(sSrc)
+        return;
+    end
+    if (nargin < 3) || isempty(fieldNames)
+        fieldNames = fieldnames(sSrc);
+    end
+    for i = 1:length(fieldNames)
+        if isfield(sSrc, fieldNames{i})
+            sDest.(fieldNames{i}) = sSrc.(fieldNames{i});
+        end
+    end
+end
