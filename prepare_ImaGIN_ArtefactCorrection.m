5961166986f77fdcb5b2429ba8a1748867a2f7c7
JD <jeandidier.lemarechal@icm-institute.org>
2018-02-02 12:15:33 +0100

    comment output info about parameters

--- prepare_ImaGIN_ArtefactCorrection.m
+++ prepare_ImaGIN_ArtefactCorrection.m
@@ -1,12 +1,12 @@
 function prepare_ImaGIN_ArtefactCorrection(Method, EventType, StartInterpolation, EndInterpolation, FileIn, FileOut)
 
-fprintf( 1, [ 'prepare_ImaGIN_ArtefactCorrection branch artefact_correction_jd\n' ] ) ;
-fprintf( 1, [ 'Method\n' ] ) ; Method
-fprintf( 1, [ 'EventType\n' ] ) ; EventType
-fprintf( 1, [ 'StartInterpolation\n' ] ) ; StartInterpolation
-fprintf( 1, [ 'EndInterpolation\n' ] ) ; EndInterpolation
-fprintf( 1, [ 'FileIn\n' ] ) ; FileIn
-fprintf( 1, [ 'FileOut\n' ] ) ; FileOut
+% fprintf( 1, [ 'prepare_ImaGIN_ArtefactCorrection branch artefact_correction_jd\n' ] ) ;
+% fprintf( 1, [ 'Method\n' ] ) ; Method
+% fprintf( 1, [ 'EventType\n' ] ) ; EventType
+% fprintf( 1, [ 'StartInterpolation\n' ] ) ; StartInterpolation
+% fprintf( 1, [ 'EndInterpolation\n' ] ) ; EndInterpolation
+% fprintf( 1, [ 'FileIn\n' ] ) ; FileIn
+% fprintf( 1, [ 'FileOut\n' ] ) ; FileOut
 
 S.Fname=FileIn ;
 
@@ -20,7 +20,7 @@ if ischar(EndInterpolation)
     EndInterpolation=str2num(EndInterpolation);
 end
 S.EndInterpolation = EndInterpolation ;
-fprintf( 1, [ 'S\n' ] ) ; S
+% fprintf( 1, [ 'S\n' ] ) ; S
 D=ImaGIN_InterpolationFilter(S) ;
 
 move( D, FileOut ) ;
