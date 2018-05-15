50acba4672d027dd720c56292e0bb6f2586afc71
Maciej Jedynak <maciej.jedynak@protonmail.com>
2018-05-15 15:22:41 +0200

    debug test

--- toolbox/set_final_status.m
+++ toolbox/set_final_status.m
@@ -1,5 +1,6 @@
 function set_final_status(status)
     if ((strcmp(status, 'OK')) || (strcmp(status, 'NOK')))
         fprintf('\n\nFINAL_STATUS: %s \n', status)
+        fprintf('DEBUG TEST MESSAGE')
     end
 end
