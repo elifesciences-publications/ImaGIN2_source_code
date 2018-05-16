a56dcf49ebf0560c3441889906fadacd8a0c8197
Maciej Jedynak <maciej.jedynak@protonmail.com>
2018-05-16 10:23:09 +0200

    new line

--- toolbox/private/set_final_status.m
+++ toolbox/private/set_final_status.m
@@ -1,5 +1,5 @@
 function set_final_status(status)
     if ((strcmp(status, 'OK')) || (strcmp(status, 'NOK')))
-        fprintf('FINAL_STATUS: %s \n', status)
+        fprintf('\nFINAL_STATUS: %s \n', status)
     end
 end
