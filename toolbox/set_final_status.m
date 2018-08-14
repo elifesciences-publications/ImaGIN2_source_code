d30e6e32f239ca0c6a71676b9357298f2c27f7c4
Maciej Jedynak <maciej.jedynak@protonmail.com>
2018-08-14 15:10:18 +0200

    removed debug message

--- toolbox/set_final_status.m
+++ toolbox/set_final_status.m
@@ -1,6 +1,5 @@
 function set_final_status(status)
     if ((strcmp(status, 'OK')) || (strcmp(status, 'NOK')))
         fprintf('\n\nFINAL_STATUS: %s \n', status)
-        fprintf('DEBUG TEST MESSAGE')
     end
 end 
